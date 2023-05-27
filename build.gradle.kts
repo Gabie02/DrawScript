4272ec17a0aad7561c18fd6b139b4153dafba9e7
Gabie02 <gpjle@iscte-iul.pt>
2023-05-27 21:24:29 +0100

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("script-runtime"))
+    implementation("org.antlr:antlr4:4.11.1")
+    implementation("com.github.javaparser:javaparser-symbol-solver-core:3.25.1")
+    implementation("org.ow2.asm:asm:9.4")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
